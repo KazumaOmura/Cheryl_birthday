43ad147b8a015fed4fd78017a9e6cb825e7afee4
KazumaOmura <kazumaxbaseball123@gmail.com>
2021-06-23 00:15:31 +0900

    修正

--- js/index.js
+++ js/index.js
@@ -2,25 +2,28 @@
         var hit = document.getElementById('hit');
         var reset = document.getElementById('reset'); 
         var stand = document.getElementById('stand');
-        var your_card = document.getElementById('your_card');
-        var com_card =  document.getElementById('com_card');
         var your_sum = document.getElementById('your_sum');
         var com_sum = document.getElementById('com_sum');
         var form = document.getElementById('form');
-        var com_sum_process = 0;
         var innner_candidate_month = document.getElementById('candidate_month');
         var innner_candidate_day = document.getElementById('candidate_day');
+
+
+        // 選択できる月日を格納する関数
+        function Select_birthdays(month, day){
+          this.month = month;
+          this.day = day;
+        };
+        var select_birthdays = [];
+        let x=0;
 
 
 
 
         // 月の生成
-        let flag = false; // 判定済フラグ
-
-        // 月の初期化
-        let candidate_month = new Array(4);
+        let candidate_month = new Array(4); // 月配列の初期化
       
-        let duplicate_judge = "false";
+        let duplicate_judge = "false"; // 判定済フラグ
         while(duplicate_judge == "false"){
           candidate_month[0] = Math.floor( Math.random() * 12 );
           if(0 != candidate_month[0]){
@@ -30,7 +33,7 @@
       
         for(let n=1; n<4; n++){
           // 重複判定
-          let duplicate_judge = "false";
+          duplicate_judge = "false";
           while(duplicate_judge == "false"){
             candidate_month[n] = Math.floor( Math.random() * 12 ) ;
             if(candidate_month[n-1] != candidate_month[n] && candidate_month[n-2] != candidate_month[n] && candidate_month[n-3] != candidate_month[n] && candidate_month[n-4] != candidate_month[n] && 0 != candidate_month[n]){
@@ -39,16 +42,6 @@
           }
         }   
         for(let n=0; n<4; n++){
-          // 並び替え
-          candidate_month.sort(function(first, second){
-            if (first > second){
-              return 1;
-            }else if (first < second){
-              return -1;
-            }else{
-              return 0;
-            }
-          });
           var com_box = document.createElement("td"); 
           var com_hand = document.createTextNode(candidate_month[n]+"月 ");
           innner_candidate_month.appendChild(com_box);
@@ -56,12 +49,9 @@
         }
 
         // 日の生成
-        flag = false; // 判定済フラグ
-
-        // 日の初期化
-        let candidate_day = new Array(3);
+        let candidate_day = new Array(3); // 日配列の初期化
       
-        duplicate_judge = "false";
+        duplicate_judge = "false"; // 判定済フラグ
         while(duplicate_judge == "false"){
           candidate_day[0] = Math.floor( Math.random() * 28 );
           if(0 != candidate_day[0]){
@@ -71,7 +61,7 @@
       
         for(let n=1; n<6; n++){
           // 重複判定
-          let duplicate_judge = "false";
+          duplicate_judge = "false";
           while(duplicate_judge == "false"){
             candidate_day[n] = Math.floor( Math.random() * 28 ) ;
             if(candidate_day[n-1] != candidate_day[n] && candidate_day[n-2] != candidate_day[n] && candidate_day[n-3] != candidate_day[n] && candidate_day[n-4] != candidate_day[n] && 0 != candidate_day[n]){
@@ -104,16 +94,13 @@
 
 
         // 最初に消去する日を2つ選ぶ
-        flag = false; // 判定済フラグ
-
-        // 選ぶ日の引数初期化
-        let choice_day = new Array(2);
+        let choice_day = new Array(2);  // 選ぶ日の引数初期化
       
-        duplicate_judge = "false";
+        duplicate_judge = "false";  // 判定済フラグ
         choice_day[0] = Math.floor( Math.random() * 6 );
         for(let n=1; n<2; n++){
           // 重複判定
-          let duplicate_judge = "false";
+          duplicate_judge = "false";
           while(duplicate_judge == "false"){
             choice_day[n] = Math.floor( Math.random() * 6 ) ;
             if(choice_day[n-1] != choice_day[n]){
@@ -129,16 +116,12 @@
 
 
         // 最初に消去する月を2つ選ぶ
-        flag = false; // 判定済フラグ
-
-        // 選ぶ月の引数初期化
-        let choice_month = new Array(2);
+        let choice_month = new Array(2);  // 選ぶ月の引数初期化
       
-        duplicate_judge = "false";
         choice_month[0] = Math.floor( Math.random() * 4 );
         for(let n=1; n<2; n++){
           // 重複判定
-          let duplicate_judge = "false";
+          duplicate_judge = "false";
           while(duplicate_judge == "false"){
             choice_month[n] = Math.floor( Math.random() * 4 ) ;
             if(choice_month[n-1] != choice_month[n]){
@@ -151,12 +134,9 @@
 
 
         // 残った4つ日から1つを除外する日を選ぶ
-        flag = false; // 判定済フラグ
+        let choice_day2 = new Array(1); // 選ぶ日の引数初期化
 
-        // 選ぶ日の引数初期化
-        let choice_day2 = new Array(1);
-
-        duplicate_judge = "false";
+        duplicate_judge = "false";  // 判定済フラグ
 
         while(duplicate_judge == "false"){
           choice_day2 = Math.floor( Math.random() * 6 ) ;
@@ -170,12 +150,9 @@
 
 
           // 正解の日を選ぶ
-          flag = false; // 判定済フラグ
+          let collect_day;  // 選ぶ日の引数初期化
 
-          // 選ぶ日の引数初期化
-          let collect_day;
-
-          duplicate_judge = "false";
+          duplicate_judge = "false";  // 判定済フラグ
 
           while(duplicate_judge == "false"){
             collect_day = Math.floor( Math.random() * 6 ) ;
@@ -184,7 +161,6 @@
             }
           }
 
-          console.log(candidate_day[collect_day]);
 
 
 
@@ -196,65 +172,159 @@
 
         // 確定した選択肢の表示処理
         let n2=0;
+        let collect_month_flag="false";
         for(let n=0; n<4; n++){
+
+          if(choice_month[n2] == n){
+
+            //　誕生日の選択肢を追加する
+            function init(){
+              select_birthdays[x] = new Select_birthdays(candidate_month[n], candidate_day[choice_day[n2]]);
+              x++;
+            };init();
+
+            n2++;
+          }
+          else{
+            if(collect_month_flag == "false"){
+              // 正解の月を確定
+              var collect_month = n;
+
+              collect_month_flag = "ture";
+
+              //　誕生日の選択肢を追加する
+              function init(){
+                select_birthdays[x] = new Select_birthdays(candidate_month[n], candidate_day[collect_day]);
+                x++;
+              };init(); 
+            }
+
+              //　誕生日の選択肢を追加する
+              function init(){
+                select_birthdays[x] = new Select_birthdays(candidate_month[n], candidate_day[choice_day2]);
+                x++;
+              };init(); 
+          }
+        }
+
+         // 最後に出力する不正解の日の引数確定
+         let choice_day3 = Array(3);
+         let i = 0;
+         for(let n=0; n<6; n++){
+          if(choice_day[0] != n && choice_day[1] != n && collect_day != n){
+            choice_day3[i] = n;
+            i++;
+          }
+         }
+
+
+        for(let n=0; n<4; n++){
+          if(choice_month[0] != n && choice_month[1] != n && collect_month != n){
+            for(let i=0; i<2; i++){
+
+              //　誕生日の選択肢を追加する
+              function init(){
+                select_birthdays[x] = new Select_birthdays(candidate_month[n], candidate_day[choice_day3[i]]);
+                x++;
+              };init(); 
+            }
+          }
+        }
+
+
+
+
+
+        // 最初に除外した月に正解日と最後に確定した除外する2つの日をランダムで散らばせる
+        let random = Math.floor( Math.random() * 2) + 1; //割合を決める
+        if(random == 1){
+
+          // 正解の誕生日が含まれている引数を格納する
+          var collect_birthday_num = x;
+
+          //　誕生日の選択肢を追加する
+          function init(){
+            select_birthdays[x] = new Select_birthdays(candidate_month[choice_month[1]], candidate_day[collect_day]);
+            x++;
+          };init(); 
+
+          for(let i=0; i<random+1; i++){
+
+            //　誕生日の選択肢を追加する
+            function init(){
+              select_birthdays[x] = new Select_birthdays(candidate_month[choice_month[0]], candidate_day[choice_day3[i]]);
+              x++;
+            };init(); 
+          }
+        }
+          else if(random == 2){
+            for(let i=0; i<random; i++){
+
+            //　誕生日の選択肢を追加する
+            function init(){
+              select_birthdays[x] = new Select_birthdays(candidate_month[choice_month[0]], candidate_day[choice_day3[i]]);
+              x++;
+            };init(); 
+          }
+
+
+            // 正解の誕生日が含まれている引数を格納する
+            var collect_birthday_num = x;
+
+            //　誕生日の選択肢を追加する
+            function init(){
+              select_birthdays[x] = new Select_birthdays(candidate_month[choice_month[1]], candidate_day[collect_day]);
+              x++;
+            };init(); 
+          }
+
+
+          
+          // 月でソート実行
+          // select_birthdays.sort(function(a, b){
+          //   if (a.month > b.month) return 1;
+          //   if (a.month < b.month) return -1;
+          //   return 0;
+          // });
+
+
+
+          
+
+          console.log("正解の誕生月");
+            console.log(select_birthdays[collect_birthday_num].month);
+            console.log("正解の誕生月");
+            console.log("正解の誕生ひ");
+            console.log(select_birthdays[collect_birthday_num].day);
+            console.log("正解の誕生ひ");
+
+
+
+            var parent = document.getElementById('choices');
+          var input_data = document.createElement('input');
+          input_data.type = 'radio';
+          input_data.id = 'inputform_' + 1;
+          parent.appendChild(input_data);
+              
+          var com_hand = document.createTextNode(select_birthdays[collect_birthday_num].month +"月"+select_birthdays[collect_birthday_num].day +"日 ⇦正解の回答です");
+          parent.appendChild(com_hand);
+
+          var br = document.createElement("br");
+          parent.appendChild(br);
+
+        for(let n=0; n<15; n++){
           var parent = document.getElementById('choices');
           var input_data = document.createElement('input');
           input_data.type = 'radio';
           input_data.id = 'inputform_' + 1;
           parent.appendChild(input_data);
-
-          // 並び替え
-          choice_month.sort(function(first, second){
-            if (first > second){
-              return 1;
-            }else if (first < second){
-              return -1;
-            }else{
-              return 0;
-            }
-          });
-
-          if(choice_month[n2] == n){
-            var com_hand = document.createTextNode(candidate_month[n] +"月"+candidate_day[choice_day[n2]] +"日");
-            parent.appendChild(com_hand);
-
-            var br = document.createElement("br");
-            parent.appendChild(br); 
-            n2++; 
-          }
-          else{
-            var com_hand = document.createTextNode(candidate_month[n] +"月"+candidate_day[choice_day2] +"日");
-            parent.appendChild(com_hand);
-
-            var br = document.createElement("br");
-            parent.appendChild(br); 
-          }
-        }
-
-
-        // 全通りの表示処理
-        for(let n=0; n<4; n++){
-          var parent = document.getElementById('target');
-          for(let n2=0; n2<6; n2++){
-          var input_data = document.createElement('input');
-          input_data.type = 'radio';
-          input_data.id = 'inputform_' + 1;
-          parent.appendChild(input_data);
-
-          var com_hand = document.createTextNode(candidate_month[n] +"月"+candidate_day[n2]+"日");
+              
+          var com_hand = document.createTextNode(select_birthdays[n].month +"月"+select_birthdays[n].day +"日");
           parent.appendChild(com_hand);
 
           var br = document.createElement("br");
           parent.appendChild(br);
-          }
         }
-
-
-
-
-
-
-
 
 
 
@@ -280,7 +350,7 @@
           let answer;
         
           //1番目の数値生成
-          let duplicate_judge = "false";
+          duplicate_judge = "false";
           while(duplicate_judge == "false"){
             candidate_month[0] = Math.floor( Math.random() * 12 );
             if(0 != candidate_month[0]){
@@ -293,7 +363,7 @@
           for(let n=1; n<4; n++){
         
             // 重複判定
-            let duplicate_judge = "false";
+            duplicate_judge = "false";
             while(duplicate_judge == "false"){
               candidate_month[n] = Math.floor( Math.random() * 12 ) ;
               if(candidate_month[n-1] != candidate_month[n] && candidate_month[n-2] != candidate_month[n] && candidate_month[n-3] != candidate_month[n] && candidate_month[n-4] != candidate_month[n] && 0 != candidate_month[n]){
